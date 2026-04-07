f2588c79db5f746a8915f528e829b823692661e7
home <atosfiel@gmail.com>
2026-04-07 23:51:25 +0100

    threads

--- src/main/java/br/com/java/threads/Threads.java
+++ src/main/java/br/com/java/threads/Threads.java
@@ -16,11 +16,17 @@ class MyRunnable implements Runnable{
 public class Threads {
     public static void main(String[] args) {
         MyThread thread = new MyThread();
+        //stage
+        System.out.println("Main thread is "+thread.getState());
         thread.start();
         System.out.println("Main thread is running.");
+        System.out.println("Main thread is "+thread.getState());
 
         MyRunnable runnable = new MyRunnable();
         Thread myThread = new Thread(runnable);
+        System.out.println("Main thread is "+myThread.getState());
         myThread.start();
+        System.out.println("Main thread is running.");
+        System.out.println("Main thread is "+myThread.getState());
     }
 }
