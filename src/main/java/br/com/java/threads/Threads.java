0c171dd27ef95988ce7f008d497ad7f2477bcec8
home <atosfiel@gmail.com>
2026-04-08 23:40:24 +0100

    threads

--- src/main/java/br/com/java/threads/Threads.java
+++ src/main/java/br/com/java/threads/Threads.java
@@ -1,4 +1,14 @@
 package br.com.java.threads;
+class Counter {
+private int count = 0;
+public synchronized void increment(){
+    count++;
+}
+
+public int getCount() {
+    return count;
+}
+}
 
 class MyThread extends Thread{
     @Override
@@ -14,7 +24,7 @@ class MyRunnable implements Runnable{
     }
 }
 public class Threads {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MyThread thread = new MyThread();
         //stage
         System.out.println("Main thread is "+thread.getState());
@@ -28,5 +38,39 @@ public class Threads {
         myThread.start();
         System.out.println("Main thread is running.");
         System.out.println("Main thread is "+myThread.getState());
+
+        Counter counter = new Counter();
+
+        Thread thread1 = new Thread(() -> {
+            System.out.println("Thread " + Thread.currentThread().getName() + " is increment.");
+            for (int i = 0; i < 2000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            System.out.println("Thread " + Thread.currentThread().getName() + " is increment.");
+            for (int i = 0; i < 2000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread thread3 = new Thread(() -> {
+            System.out.println("Thread " + Thread.currentThread().getName() + " is increment.");
+            for (int i = 0; i < 2000; i++) {
+                counter.increment();
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        thread1.join();// neste ponto o programa espera a thread1 terminar para continuar a execução, garantindo que o resultado final seja consistente
+        thread2.join();// neste ponto o programa espera a thread2 terminar para continuar a execução, garantindo que o resultado final seja consistente
+        thread3.join();// neste ponto o programa espera a thread3 terminar para continuar a execução, garantindo que o resultado final seja consistente
+
+        System.out.println("Final count: " + counter.getCount());
+
     }
 }
