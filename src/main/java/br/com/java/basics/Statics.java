e224b35e052cd53d5b9366333dad74bb9d8bc232
home <atosfiel@gmail.com>
2026-04-21 23:35:20 +0100

    add question static

---
+++ src/main/java/br/com/java/basics/Statics.java
@@ -0,0 +1,23 @@
+package br.com.java.basics;
+class Course{
+    String name;
+    //static String name;// that action below code to compile
+
+    public void getName(String name) {
+        this.name = name;
+        System.out.print(name);
+
+    }
+
+    public static void getName(){
+        name = "Java name";//Non-static field 'name' cannot be referenced from a static context
+        System.out.print(name);
+
+    }
+}
+public class Statics {
+    public static void main(String[] args) {
+        var c = new Course();
+        c.getName("Java");
+    }
+}
